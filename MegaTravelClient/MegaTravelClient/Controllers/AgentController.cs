60f28857e5ae73753de9d9901f919340abdac205
Tiffany Ford <me@tiffanyford.dev>
2022-10-24 15:57:50 -0500

    updates to add file download

--- MegaTravelClient/MegaTravelClient/Controllers/AgentController.cs
+++ MegaTravelClient/MegaTravelClient/Controllers/AgentController.cs
@@ -3,12 +3,15 @@ using MegaTravelClient.Models;
 using MegaTravelClient.Utility;
 using Newtonsoft.Json;
 using Microsoft.AspNetCore.Authorization;
+using System.Web;
 
 namespace MegaTravelClient.Controllers
 {
    // [Authorize]
     public class AgentController : Controller
     {
+        public static List<TripData> staticTripList;
+
         public IActionResult Index(LoginResponseModel userData)
         {
             return View();
@@ -18,8 +21,6 @@ namespace MegaTravelClient.Controllers
         public async Task<IActionResult> AllTripsView([FromQuery(Name = "agentID")] int agentID)
         {
 
-            List<GetTripsResponseModel> userList = new List<GetTripsResponseModel>();
-
             GetTripsResponseModel ResponseModel = null;
             try
             {
@@ -27,12 +28,37 @@ namespace MegaTravelClient.Controllers
                 ServiceHelper objService = new ServiceHelper();
                 string response = await objService.GetRequest(strSerializedData, ConstantValues.GetAllTripsForAgent + "?agentID=" + agentID, false, string.Empty).ConfigureAwait(true);
                 ResponseModel = JsonConvert.DeserializeObject<GetTripsResponseModel>(response);
+
+                staticTripList = new List<TripData>();
+
+                foreach (var item in ResponseModel.tripList)
+                {
+                    staticTripList.Add(item);
+                }
+                
+
             }
             catch (Exception ex)
             {
                 Console.WriteLine("AllTripsView API " + ex.Message);
             }
             return View(ResponseModel);
+        }
+
+        public ActionResult DownloadTrips()
+        {
+            using (MemoryStream stream = new MemoryStream())
+            {
+                StreamWriter objstreamwriter = new StreamWriter(stream);
+                foreach (TripData trip in staticTripList)
+                {
+                    objstreamwriter.WriteLine(trip.AgentId + "," + trip.Agent + "," + trip.TripName);
+                }   
+
+                objstreamwriter.Flush();
+                objstreamwriter.Close();
+                return File(stream.ToArray(), "text/plain", "trips.csv");
+            }
         }
     }
 }
