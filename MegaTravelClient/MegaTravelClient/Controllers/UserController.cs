c1fc83a69b247734be136bdd61fe0f0683798203
Tiffany Ford <me@tiffanyford.dev>
2023-09-11 07:26:08 -0500

    Remove authorization from user controller

--- MegaTravelClient/MegaTravelClient/Controllers/UserController.cs
+++ MegaTravelClient/MegaTravelClient/Controllers/UserController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace MegaTravelClient.Controllers
 {
-    [Authorize]
+
     public class UserController : Controller
     {
         public IActionResult Index(LoginResponseModel userData)
